c04a05a4c83f439e6a0510e6ceee9bac7d8edad8
Jereh Tejano <tntperpetualcup@gmail.com>
2019-04-04 10:54:04 +0800

    #update merge

--- app/src/main/java/com/example/android/tbcare_capstone/MainActivity.java
+++ app/src/main/java/com/example/android/tbcare_capstone/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //hi
         //hi this is dev_jereh
-
         //hi my name is gwen
 
         //hello
