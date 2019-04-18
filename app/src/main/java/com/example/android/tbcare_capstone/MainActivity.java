cbc6b2580b77e4b24b61fb3ba66e2faedcc92824
Jereh Tejano <tntperpetualcup@gmail.com>
2019-04-18 19:12:02 +0800

    #update code for encryption. dont do anything to this update

--- app/src/main/java/com/example/android/tbcare_capstone/MainActivity.java
+++ app/src/main/java/com/example/android/tbcare_capstone/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.android.tbcare_capstone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
+import android.widget.Toast;
+
+import java.security.MessageDigest;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 
 public class MainActivity extends AppCompatActivity {
+
+    String sample_encrypt;
+    String AES = "AES";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,7 +22,32 @@ public class MainActivity extends AppCompatActivity {
         //hi
         //hi this is dev_jereh
         //hi my name is gwen
-
+        try {
+            sample_encrypt = encrypt("", "tejanojereh");
+            Toast.makeText(this, sample_encrypt, Toast.LENGTH_LONG).show();
+            sample_encrypt = encrypt("123", "tejanojereh");
+            Toast.makeText(this, sample_encrypt, Toast.LENGTH_LONG).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         //hello
+    }
+
+    private String encrypt(String data, String password)throws Exception{
+        SecretKeySpec key = generateKey(password);
+        Cipher c = Cipher.getInstance(AES);
+        c.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encval = c.doFinal(data.getBytes());
+        String encryptedValue = Base64.encodeToString(encval, Base64.DEFAULT);
+        return encryptedValue;
+    }
+
+    private SecretKeySpec generateKey(String password) throws Exception{
+        final MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] bytes = password.getBytes("UTF-8");
+        digest.update(bytes, 0, bytes.length);
+        byte[] key = digest.digest();
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        return secretKeySpec;
     }
 }
