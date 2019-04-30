4ce4dc477d26c1422a0f690a5ddd6370677019e2
Gwen Escarda <gwenescarda0521@gmail.com>
2019-04-30 15:40:57 +0800

    #update design not finished

--- app/src/main/java/com/example/android/tbcare_capstone/MainActivity.java
+++ app/src/main/java/com/example/android/tbcare_capstone/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.tbcare_capstone;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +18,15 @@ public class MainActivity extends AppCompatActivity {
         //hi my name is gwen
 
         //hello
+
+       // button = (Button) findViewById(R.id.button2);
+       // button.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+          //  public void onClick(View v) {
+         //       Intent intent = new Intent(MainActivity.this, menu_patient.class );
+      //      }
+       // });
+
+
     }
 }
---
+++ app/src/main/java/com/example/android/tbcare_capstone/My_schedule_patient.java
@@ -0,0 +1,14 @@
+package com.example.android.tbcare_capstone;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class My_schedule_patient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_schedule_patient);
+    }
+
+}
