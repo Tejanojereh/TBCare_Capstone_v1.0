243427180471a80f839efe4508a13e6f3d202ebc
Gwen Escarda <gwenescarda0521@gmail.com>
2019-05-03 09:53:43 +0800

    #update design Register patient and partner with spinner (not finished),Selection of account registered

--- app/src/main/java/com/example/android/tbcare_capstone/RegisterActivity.java
+++ app/src/main/java/com/example/android/tbcare_capstone/RegisterActivity.java
@@ -3,16 +3,22 @@ package com.example.android.tbcare_capstone;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText txtname,txtemail,txtpass,txtcpass;
     private Button btnregister;
     private ProgressBar loading;
-
+    Spinner spinner1;
+    private Button btnSubmit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +29,19 @@ public class RegisterActivity extends AppCompatActivity {
         txtpass = findViewById(R.id.input_password);
         txtcpass = findViewById(R.id.input_confirmpassword);
         btnregister = findViewById(R.id.btnregister);
+        spinner1 = (Spinner)findViewById(R.id.spinner2);
+
+        ArrayList<String> list = new ArrayList<>();
+        list.add("What was your childhood nickname?");
+        list.add("What is your maternal grandmother's maiden name?");
+        list.add("In what city or town did your mother and father meet?");
+        list.add("Who is your childhood sports hero?");
+        list.add("What was your childhood nickname?");
+
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_dropdown_item,list);
+        spinner1.setAdapter(adapter );
 
         btnregister.setOnClickListener(new View.OnClickListener() {
             @Override
